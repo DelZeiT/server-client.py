1d02e5e0afaf29c581807316e575241e9fa21b31
DelZeiT <maksimcvetkov1106@gmail.com>
2023-06-08 23:19:24 +0300

    Добавил вывод сообщение "Подключен" после подключения клиента. Убрал лишний плюс при выводе сообщения клиента.

--- client1.py
+++ client1.py
@@ -3,6 +3,7 @@ import socket
 server = ("localhost", 9090)  # адрес сервера
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('localhost', 9090))
+print('Подключен')
 
 name = input('Имя: ')  # вводим имя
 
@@ -11,7 +12,7 @@ flag = True
 while flag:
     message = input()  # вводим сообщение
     if message != '':  # если сообщение не равно пустому символу
-        client.sendall((f'[{name}]: + {message}').encode('utf-8'))
+        client.sendall((f'[{name}]: {message}').encode('utf-8'))
     if message == 'выход':  # если клиент хочет выйти из чата
         client.sendall((f'[{name}] - покинул чат').encode('utf-8'))
         flag = False
