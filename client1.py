3c84872bb640bc2c839f0b6d9acbecd2fded3e5d
DelZeiT <maksimcvetkov1106@gmail.com>
2023-06-08 14:08:04 +0300

    убрал лишнюю переменную join и связанное с ней условие, при котором проверялось, подключился ли клиент к серверу.

--- client1.py
+++ client1.py
@@ -1,29 +1,18 @@
 import socket
 
-server = ("localhost", 9090)      #адрес сервера
+server = ("localhost", 9090)  # адрес сервера
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('localhost', 12953))
 
-name = input('Имя: ')       #вводим имя
+name = input('Имя: ')  # вводим имя
 
 flag = True
-join = False
 
 while flag:
-    if join == False:       #если клиент не присоединен
-        client.sendto(('[' + name + '] - присоединился к чату').encode('utf-8'), server) #
-        join = True
-    else:
-        message = input()    #вводим сообщение
-        if message != '':          #если сообщение не равно пустому символу
-            client.sendto(('[' + name + ']:' + message).encode('utf-8'), server)
-        if message == 'выход':       #если клиент хочет выйти из чата
-            client.sendto(('[' + name + '] - покинул чат').encode('utf-8'), server)
-            flag = False
+    message = input()  # вводим сообщение
+    if message != '':  # если сообщение не равно пустому символу
+        client.sendto(('[' + name + ']:' + message).encode('utf-8'), server)
+    if message == 'выход':  # если клиент хочет выйти из чата
+        client.sendto(('[' + name + '] - покинул чат').encode('utf-8'), server)
+        flag = False
 client.close()
-
-
-
-
-
-
